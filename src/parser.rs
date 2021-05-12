800f1855399d9a694920e0d2de1686eabd0bdc83
Michal Gniadek <mgniadek@interia.pl>
2021-05-12 17:24:05 +0200

    Documentation

--- src/parser.rs
+++ src/parser.rs
@@ -6,13 +6,13 @@ use crate::{
 use logos::{Lexer, Span, SpannedIter};
 use std::iter::Peekable;
 
-pub fn parse<'a>(lexer: Lexer<'a, Token>) -> UntypedAst {
+pub fn parse(lexer: Lexer<'_, Token>) -> UntypedAst {
     let mut parser = Parser {
         lexer: lexer.spanned().peekable(),
         spans: Spans::new(),
     };
 
-    let fun = parser.parse_fun();
+    let fun = parser.parse_item();
 
     UntypedAst {
         item: fun,
@@ -39,36 +39,47 @@ impl ParserResult {
     }
 }
 
-impl<'a> Parser<'a> {
+impl Parser<'_> {
     const ITEM_SYNC: [Token; 1] = [Token::Fun];
     const EXPR_SYNC: [Token; 2] = [Token::RightBrace, Token::Fun];
 
+    /// Peeks the next token.
     fn peek(&mut self) -> Option<Token> {
         self.lexer.peek().map(|(t, _)| *t)
     }
 
+    /// Peeks the next span.
     fn peek_span(&mut self) -> Span {
         self.lexer.peek().map_or(0..0, |(_, s)| s.clone())
     }
 
+    /// Advances to the next token and returns the previous span.
     fn skip(&mut self) -> Span {
         let span = self.peek_span();
         self.lexer.next();
         span
     }
 
+    /// Advances to the next token, add span to the Spans and return the
+    /// corresponding id.
     fn consume(&mut self) -> Id {
         let span = self.peek_span();
         self.lexer.next();
         self.spans.push(span)
     }
 
+    /// Advances until it peeks one of the token in tokens and adds all
+    /// the previous spans to Spans and return the corresponding id.
+    /// You should pass one of the sync tokens arrays.
     fn err_consume(&mut self, tokens: &[Token]) -> Id {
         let span = self.peek_span();
         let id = self.spans.push(span);
         self.err_consume_add_to(id, tokens)
     }
 
+    /// Advances until it peeks one of the token in tokens and adds all
+    /// the previous spans to the passed id and return the passed id.
+    /// You should pass one of the sync tokens arrays.
     fn err_consume_add_to(&mut self, id: Id, tokens: &[Token]) -> Id {
         while let Some((token, span)) = self.lexer.peek() {
             if !tokens.contains(token) {
@@ -81,8 +92,18 @@ impl<'a> Parser<'a> {
         id
     }
 
+    fn parse_item(&mut self) -> Item {
+        match self.peek() {
+            Some(Token::Fun) => self.parse_fun(),
+            _ => todo!(),
+        }
+    }
+
     fn parse_fun(&mut self) -> Item {
-        // Fun keyword
+        assert!(
+            self.peek() == Some(Token::Fun),
+            "Compiler error: parse_fun should be called with fun keyword remaining."
+        );
         let id = self.consume();
 
         let name = match self.peek() {
@@ -145,6 +166,10 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_block(&mut self) -> ParserResult {
+        assert!(
+            self.peek() == Some(Token::LeftBrace),
+            "Compiler error: parse_fun should be called with left brace keyword remaining."
+        );
         let id = self.consume();
 
         let mut exprs = Vec::new();
@@ -173,12 +198,10 @@ impl<'a> Parser<'a> {
         Ok(Expr::Block(id, exprs))
     }
 
-    fn parse_infix(&mut self, tree: Expr, min_binding_power: u8) -> ParserResult {
+    fn parse_infix(&mut self, mut tree: Expr, min_binding_power: u8) -> ParserResult {
         // Associativity
         const LEFT: u8 = 0;
         const _RIGHT: u8 = 1;
-
-        let mut tree = tree;
 
         while let Some(token) = self.peek() {
             tree = match token {

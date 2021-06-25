8fdb9df059bbffaf722b8dba6f428888930428c5
Michal Gniadek <mgniadek@interia.pl>
2021-06-25 16:56:35 +0200

    Tests don't check for unrelated errors.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,52 +1,56 @@
-use coffin2::{
-    ast::Ast,
-    debug_print, lexer,
-    name_resolution::{self, VariableTable},
-    parser, spirv_generation,
-    type_resolution::{self, types::TypeTable},
-};
+use coffin2::{debug_print, lexer, name_resolution, parser, type_resolution};
 use insta::{assert_snapshot, glob};
-use rspirv::dr::Module;
 use std::{fs, path::Path};
 
-fn test_util(path: &Path) -> (Ast, VariableTable, TypeTable, Module) {
+fn test_util(path: &Path, spans: bool, variables: bool, types: bool) -> String {
     let code = fs::read_to_string(path).unwrap();
     let lexer = lexer::lex(&code);
     let mut ast = parser::parse(lexer);
-    let vars = name_resolution::visit(&mut ast);
-    let types = type_resolution::visit(&mut ast, &vars);
-    let module = spirv_generation::visit(&mut ast, &vars, &types);
-    (ast, vars, types,   module.unwrap_or(Module::new()))
+
+    let variables = if variables | types {
+        Some(name_resolution::visit(&mut ast))
+    } else {
+        None
+    };
+
+    let types = if types {
+        Some(type_resolution::visit(
+            &mut ast,
+            variables.as_ref().unwrap(),
+        ))
+    } else {
+        None
+    };
+
+    // let _module = spirv_generation::visit(&mut ast, &vars, &type_table);
+
+    debug_print::visit(&ast, true, spans, variables.as_ref(), types.as_ref())
 }
 
 #[test]
 fn parser() {
     glob!(r"insta_parser\*.coff", |path| {
-        let (ast, _, _, _) = test_util(path);
-        assert_snapshot!(debug_print::visit(&ast, true, false, None, None));
+        assert_snapshot!(test_util(path, false, false, false));
     });
 }
 
 #[test]
 fn parser_spans() {
     glob!(r"insta_parser_spans\*.coff", |path| {
-        let (ast, _, _, _) = test_util(path);
-        assert_snapshot!(debug_print::visit(&ast, true, true, None, None));
+        assert_snapshot!(test_util(path, true, false, false));
     });
 }
 
 #[test]
 fn variables() {
     glob!(r"insta_variables\*.coff", |path| {
-        let (ast, vars, _, _) = test_util(path);
-        assert_snapshot!(debug_print::visit(&ast, true, false, Some(&vars), None));
+        assert_snapshot!(test_util(path, false, true, false));
     });
 }
 
 #[test]
 fn types() {
     glob!(r"insta_types\*.coff", |path| {
-        let (ast, _, types, _) = test_util(path);
-        assert_snapshot!(debug_print::visit(&ast, true, false, None, Some(&types),));
+        assert_snapshot!(test_util(path, false, false, true));
     });
 }
